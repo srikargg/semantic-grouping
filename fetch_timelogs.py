e1ce6756bd072347d9935d3d678ca07912f5147e
srikargg <160564360+srikargg@users.noreply.github.com>
2026-06-09 01:43:59 -0700

    Phase 3: in progress... searching for valid project ID with real time data log

--- fetch_timelogs.py
+++ fetch_timelogs.py
@@ -1,14 +1,19 @@
 import requests
 from get_token import get_access_token
 
-
 token = get_access_token()
 
-
-
 headers = {"Authorization": f"Zoho-oauthtoken {token}"}
-url = "https://projectsapi.zoho.com/restapi/portal/771456286/projects/YOUR_PROJECT_ID/logs/"
 
-response = requests.get(url, headers=headers)
+url = "https://projectsapi.zoho.com/api/v3/portal/771456286/projects/1918757000010163111/timelogs"
+
+params = {
+    "view_type": "customdate",
+    "start_date": "2025-01-01",
+    "end_date": "2025-06-30",
+    "module": '{"type":"task"}'
+}
+
+response = requests.get(url, headers=headers, params=params)
 data = response.json()
 print(data)
